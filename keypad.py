37a843e940afdab800b300044694a8b647ce9ae9
Yuntao(Winston) <yuntaowu2009@hotmail.com>
2020-02-28 10:13:03 -0800

    Update keypad.py

--- keypad.py
+++ keypad.py
@@ -4,19 +4,36 @@ import time
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(7,GPIO.OUT)   #col2 7   
-GPIO.setup(8,GPIO.OUT)  #col0 8
-GPIO.setup(25,GPIO.IN,pull_up_down = GPIO.PUD_UP)   #row1 25
+col0 = 7
+col1 = 20
+col2 = 8
+row0 = 21
+row1 = 25
+GPIO.setup(col0,GPIO.OUT)   #col0 7   
+GPIO.setup(col2,GPIO.OUT)  #col2 8
+GPIO.setup(col1,GPIO.OUT) #col1 20
+GPIO.setup(row0,GPIO.IN,pull_up_down = GPIO.PUD_UP) #row0 21
+GPIO.setup(row1,GPIO.IN,pull_up_down = GPIO.PUD_UP)   #row1 25
 
 while True:
-    GPIO.output(7,GPIO.LOW)
-    GPIO.output(8,GPIO.HIGH)
+    GPIO.output(col0,GPIO.LOW)
+    GPIO.output(col1,GPIO.HIGH)
+    GPIO.output(col2,GPIO.HIGH)
     if GPIO.input(25) == 0:
         print("4")
-    GPIO.output(7,GPIO.HIGH)
-    GPIO.output(8,GPIO.LOW)
+    GPIO.output(col0,GPIO.HIGH)
+    GPIO.output(col1,GPIO.LOW)
+    GPIO.output(col2,GPIO.HIGH)
+    if GPIO.input(25) == 0:
+        print("5")
+    elif GPIO.input(21) == 0:
+        print("2")
+
+    GPIO.output(col0,GPIO.HIGH)
+    GPIO.output(col1,GPIO.HIGH)
+    GPIO.output(col2,GPIO.LOW)
     if GPIO.input(25) == 0:
         print("6")
-
+        
     time.sleep(0.2)
 
