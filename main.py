98339f073fe8e059d4af90f3ecc1f48a4078b09d
nick-ng13 <nicholas_ng@shaw.ca>
2020-03-04 11:58:52 -0800

    compiled webserver and camera??

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 
 from adafruit_motorkit import MotorKit
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from picamera import PiCamera
 
 #############################################
 
@@ -59,6 +61,7 @@ def turn180():
 
 #############################################
 
+# setting up spi
 spi = spidev.SpiDev()
 spi.open(0,0)
 spi.max_speed_hz=1000000
@@ -67,7 +70,10 @@ spi.max_speed_hz=1000000
 lSensor = 0
 mSensor = 1
 rSensor = 2
+
+# define default speed
 moveSpeed = 0.25
+
 # Define delay between readings
 delay = 0.1
 
@@ -79,6 +85,72 @@ def ReadChannel(channel):
   return data
 
 #############################################
+
+host_name = '137.82.226.231'    # Change this to Raspberry Pi IP address
+host_port = 8000
+
+class MyServer(BaseHTTPRequestHandler):
+
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def _redirect(self, path):
+        self.send_response(303)
+        self.send_header('Content-type', 'text/html')
+        self.send_header('Location', path)
+        self.end_headers()
+
+    def do_GET(self):
+        # generate the html web with four buttons to control the robot to move forward, backward, left and right
+        html = '''
+            <html>
+            <body style="width:960px; margin: 20px auto;">
+            <h1>CPEN291 Project 1 Raspberry Pi web robot control</h1>
+            <p>Current temperature is {}</p>
+            <form action="/" method="POST">
+                Robot control: <br />
+                <input type="submit" name="submit" value="Forward"> <br />
+                <input type="submit" name="submit" value="Left">
+                <input type="submit" name="submit" value="Right"><br />
+                <input type="submit" name="submit" value="Backward"> 
+            </form>
+            </body>
+            </html>
+        '''
+        temp = os.popen("/opt/vc/bin/vcgencmd measure_temp").read()
+        self.do_HEAD()
+        self.wfile.write(html.format(temp[5:]).encode("utf-8"))
+
+    def do_POST(self):
+        # we get the request from the user
+        # call the specific function in motor functions to handle the request
+        content_length = int(self.headers['Content-Length'])        # Get the size of data
+        post_data = self.rfile.read(content_length).decode("utf-8") # Get the data
+        post_data = post_data.split("=")[1]                         # Only keep the value
+
+        if post_data == 'Forward':
+            moveForward(moveSpeed)
+            print("car is moving forward")
+        elif post_data == 'Backward':
+            moveBackward(moveSpeed)
+            print("car is moving backward")
+        if post_data =='Left':
+            rotateLeft(moveSpeed)
+            print("car is rotating left")
+        elif post_data == 'Right':
+            rotateRight(moveSpeed)
+            print("car is rotating right")
+        
+        self._redirect('/') # finished handling request, redirect back to the root url
+
+#############################################
+
+camera = PiCamera()
+
+#############################################
+
 dist = 0
 
 while True:
