087faa28ac58fedbc48fbbf47fcb0a63b32d99fb
huajustin <huajustinh@gmail.com>
2020-03-06 09:19:27 -0800

    Added some debugging info

--- bluetoothSocket.py
+++ bluetoothSocket.py
@@ -1,6 +1,6 @@
 # from https://circuitdigest.com/microcontroller-projects/controlling-raspberry-pi-gpio-using-android-app-over-bluetooth
-import Bluetooth
-import motorFunctions
+import bluetooth
+#import motorFunctions
  
 server_socket=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
 uuid = "bbcc40a1-adae-41d7-a13f-676f427c9c41"
@@ -12,11 +12,12 @@ server_socket.listen(1)
 advertise_service( server_socket, "Bluetooth Server", service_id = uuid, 
                 service_classes = [ uuid, bluetooth.SERIAL_PORT_CLASS ],profiles = [ bluetooth.SERIAL_PORT_PROFILE ]
                 )
- 
+print("Currently looking for connections...")
 client_socket,address = server_socket.accept()
 print("Accepted connection from %s" %(address))
 while 1: 
     data = client_socket.recv(1024)
+    print(data)
     break
  
 client_socket.close()
