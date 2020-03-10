f9417786098fd86dc85bcc8afa2baf4dc050436a
huajustin <huajustinh@gmail.com>
2020-03-09 17:20:12 -0700

    fixed bluetooth server for the pi

--- bluetoothSocket.py
+++ bluetoothSocket.py
@@ -4,13 +4,14 @@ import bluetooth
  
 # get server socket and set UUID and port number
 server_socket=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
-uuid = "bbcc40a1-adae-41d7-a13f-676f427c9c41"
-port = 10
+uuid = "56e8a14a-80b3-11e5-8bcf-feff819cdc9f"
+port = bluetooth.PORT_ANY
 
 # set up the server socket and listen in on the connection
 server_socket.bind(("",port))
 server_socket.listen(1)
-bluetooth.advertise_service( server_socket, "Bluetooth Server", service_id = uuid, 
+bluetooth.advertise_service( server_socket, "Bluetooth Server",
+                             service_id = uuid, 
                 service_classes = [ uuid, bluetooth.SERIAL_PORT_CLASS ],profiles = [ bluetooth.SERIAL_PORT_PROFILE ]
                 )
 print("Currently looking for connections...")
